729d6088996d573b8a7e505d593daa5e115c05ef
Sophia <dollgirl4o7@gmail.com>
2021-03-03 22:32:33 -0800

    delete card works

--- src/component/board/BoardHeader.js
+++ src/component/board/BoardHeader.js
@@ -5,21 +5,36 @@ import logo from '../../images/prairieHealthLogo.png';
 class BoardHeader extends Component {
   render() {
     return (
-        <Tablist className='board-header' marginBottom={4}>
-          <Tab className='header-tab'><AlignmentTopIcon marginRight={6} />Board<ChevronDownIcon marginLeft={6} /></Tab>
-          <Tab style={{ fontSize: '18px' }}><b>Test</b></Tab>
-          <Tab className='header-tab'><StarEmptyIcon /></Tab>
-          <Tab className='header-tab'>Prairie Health</Tab>
-          <Tab className='header-tab'>Team Visible</Tab>
-          <Tab>
-            <img src={logo}
-              style={{ width: '20px', height: '20px', borderRadius: '100px' }}
-              alt='logo' />
-          </Tab>
-          <Tab className='header-tab'>Invite</Tab>
-          <Tab className='header-tab'><HatIcon marginRight={10} />Butler</Tab>
-          <Tab className='header-tab'><MoreIcon color="muted" marginRight={10} />Show Menu</Tab>
-        </Tablist>
+      // <div className='board-header' marginBottom={4}>
+      //   <div className='header-tab'><AlignmentTopIcon marginRight={6} />Board<ChevronDownIcon marginLeft={6} /></div>
+      //   <div className='header-tab'><b>Test</b></div>
+      //   <div className='header-tab'><StarEmptyIcon /></div>
+      //   <div className='header-tab'>Prairie Health</div>
+      //   <div className='header-tab'>Team Visible</div>
+      //   <div>
+      //     <img src={logo}
+      //       className='header-tab'
+      //       alt='logo' />
+      //   </div>
+      //   <div className='header-tab'>Invite</div>
+      //   <div className='header-tab'><HatIcon marginRight={10} />Butler</div>
+      //   <div className='header-tab'><MoreIcon color="muted" marginRight={10} />Show Menu</div>
+      // </div>
+      <Tablist className='board-header' marginBottom={4}>
+        <Tab className='header-tab'><AlignmentTopIcon marginRight={6} />Board<ChevronDownIcon marginLeft={6} /></Tab>
+        <Tab style={{ fontSize: '18px' }}><b>Test</b></Tab>
+        <Tab className='header-tab'><StarEmptyIcon /></Tab>
+        <Tab className='header-tab'>Prairie Health</Tab>
+        <Tab className='header-tab'>Team Visible</Tab>
+        <Tab>
+          <img src={logo}
+            style={{ width: '20px', height: '20px', borderRadius: '100px' }}
+            alt='logo' />
+        </Tab>
+        <Tab className='header-tab'>Invite</Tab>
+        <Tab className='header-tab'><HatIcon marginRight={10} />Butler</Tab>
+        <Tab className='header-tab'><MoreIcon color="muted" marginRight={10} />Show Menu</Tab>
+      </Tablist>
     );
   }
 }
--- src/component/board/Card.js
+++ src/component/board/Card.js
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
-import EditableLabel from 'react-editable-label';
+// import EditableLabel from 'react-editable-label';
+import { CrossIcon } from 'evergreen-ui';
 
 export const Card = (props) => {
-  const [taskText, setTaskText] = useState(props.taskText)
+  const [taskText, setTaskText] = useState(props.taskText);
+  // console.log(props);
 
-  const saveTaskText = (value) => {
-    setTaskText(value);
+  // const saveTaskText = (value) => {
+  //   setTaskText(value);
 
-    props.editTaskText(value, props.listNumber)
+  //   props.editTaskText(value, props.listNumber)
+  // }
+
+  const deleteTask = () => {
+    console.log(props.listNumber);
+    console.log(props.timeId);
+    props.deleteCard(props.listNumber, props.timeId);
   }
-  
+
   return (
     <div className='card'>
-      <EditableLabel initialValue={taskText} save={value => saveTaskText(value)} />
+      {/* <EditableLabel initialValue={taskText} save={value => saveTaskText(value)} /> */}
+      <label style={{ marginRight: '50px' }}>{taskText}</label>
+      <button onClick={deleteTask}><CrossIcon/></button>
     </div>
   );
 }

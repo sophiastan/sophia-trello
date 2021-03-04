d11d730f819c551727684cac1224c07848ae5a6c
Sophia <dollgirl4o7@gmail.com>
2021-03-04 03:23:31 -0800

    butler moves to the side

--- src/component/board/BoardHeader.js
+++ src/component/board/BoardHeader.js
@@ -4,13 +4,23 @@ import logo from '../../images/prairieHealthLogo.png';
 // import { slide as Menu } from 'react-burger-menu';
 
 class BoardHeader extends Component {
-
-  openNav() {
-    document.getElementById('mySidenav').style.width = '339px';
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false
+    }
   }
 
-  closeNav() {
-    document.getElementById('mySidenav').style.width = '0';
+  openNav = () => {
+    this.setState({
+      showMenu: true
+    });
+  }
+
+  closeNav = () => {
+    this.setState({
+      showMenu: false
+    });
   }
 
   render() {
@@ -31,18 +41,22 @@ class BoardHeader extends Component {
         <div style={{ flexGrow: '1' }}></div>
         <div style={{ display: 'flex' }}>
           <Tab className='header-tab'><HatIcon marginRight={10} />Butler</Tab>
-          <Tab className='header-tab' onClick={this.openNav}>
-            <MoreIcon color='muted' marginRight={10} />Show Menu
-          </Tab>
-          <div id='mySidenav' className='board-menu'>
-            <div className='board-menu-header'>
-              <h3 className='board-menu-header-title'>Menu</h3>
-              <div className='closebtn' onClick={this.closeNav}>&times;</div>
+          {!this.state.showMenu ?
+            <Tab className='header-tab' onClick={this.openNav}>
+              <MoreIcon color='muted' marginRight={10} />Show Menu
+              </Tab>
+            :
+            <div id='mySidenav' className='board-menu'>
+              <div className='board-menu-header'>
+                <h3 className='board-menu-header-title'>Menu</h3>
+                <div className='closebtn' onClick={this.closeNav}>&times;</div>
+              </div>
+              <hr className='board-menu-header-divider' />
+              <div className='board-menu-content'>
+                <p>HELLO</p>
+              </div>
             </div>
-            <hr className='board-menu-header-divider' />
-            <div className='board-menu-content'>
-            </div>
-          </div>
+          }
         </div>
       </Tablist>
     );

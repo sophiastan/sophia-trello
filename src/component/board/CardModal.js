674cfeea25675cf5adbdceb84a699164ca2460de
Sophia <dollgirl4o7@gmail.com>
2021-03-05 13:55:18 -0800

    cleanup

--- src/component/board/CardModal.js
+++ src/component/board/CardModal.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Dialog, Pane } from 'evergreen-ui';
 import EditableLabel from 'react-editable-label';
 import { IoArchiveOutline } from 'react-icons/io5';
 import Modal from 'react-modal';
@@ -11,11 +10,6 @@ export const CardModal = (props) => {
   }
 
   return (
-    // <div className='board-menu'>
-    //   <div className='closebtn' onClick={onShowModal}>&times;</div>
-    //   <h3>hi</h3>
-    //   <h2>saved</h2>
-    // </div>
     <Modal
       isOpen={props.isShown}
       style={{
@@ -24,11 +18,9 @@ export const CardModal = (props) => {
         },
         content: {
           display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-evenly'
+          flexDirection: 'column'
         }
-      }}
-    >
+      }}>
       <label style={{ marginBottom: '8px' }}><b>Change Card Name: </b></label>
       <EditableLabel initialValue={props.taskText}
         save={value => props.saveTaskText(value)} />
@@ -37,12 +29,6 @@ export const CardModal = (props) => {
           </button>
       <button onClick={onShowModal}>close</button>
     </Modal>
-    // <div className='modal'>
-    //   <div className='modal-content'>
-    //     <span className='close' >&times;</span>
-    //     <p>Some text in Modal</p>
-    //   </div>
-    // </div>
   )
 }
 
